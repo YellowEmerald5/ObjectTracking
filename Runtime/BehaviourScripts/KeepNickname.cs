3b93e18a709dfbaad469a10120bd4d7f9a927d84
Mathias Nupen <584967@stud.hvl.no>
2024-10-14 12:40:50 +0200

    Added script to show what user is active
    The nickname input will now show the nickname given in the application instead of being empty after completing a game

---
+++ Runtime/BehaviourScripts/KeepNickname.cs
@@ -0,0 +1,18 @@
+﻿using ScriptableObjectScripts;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace BehaviourScripts
+{
+    public class KeepNickname : MonoBehaviour
+    {
+        [SerializeField] public TMP_InputField nicknameInput;
+        [SerializeField] public StorageSO storage;
+
+        private void Awake()
+        {
+            nicknameInput.text = storage.nickname;
+        }
+    }
+}
